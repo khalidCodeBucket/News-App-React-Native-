2fb5e36bd79904218971e62169febf432eda0677
khalidCodeBucket <khalid.mahmud2014@gmail.com>
2020-03-12 14:13:39 +0600

    React Native News App

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import TabScreen from './src/screens/TabScreen';
+
+export default class App extends Component {
+    render() {
+        return (
+            <TabScreen />
+        );
+    }
+}
+
---
+++ src/config/rest_config.js
@@ -0,0 +1,4 @@
+export const articles_url = 'http://newsapi.org/v2/top-headlines';
+export const country_code = 'us';
+export const category = 'general';
+export const _api_key = '4b9ab2c6cccb4346991eea1d91fc630c';
